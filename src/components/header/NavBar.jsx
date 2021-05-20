82a4788a2b5ebda3ac3c0dd15eea0225bbdb9c6f
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-05-20 19:53:27 +0530

    inital commit

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar  = () =>{
+	
+	
+
+	return(
+		<div className="main">
+			<div className="nav"></div>
+			<div className="nav"> Hello</div>
+	<div className="nav"> Hello</div>
+	<div className="nav"> Hello</div>
+	<div className="nav"> Hello</div>
+	<div className="nav"> Hello</div>
+	<div className="nav"> Hello</div>
+	<div className="nav"></div>
+		</div>
+	
+	)
+}
+
+export default NavBar;
---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/header/NavBar";
+
+const MainPage = ()=>{
+
+	return(
+			<NavBar/>
+	)
+}
+
+export default MainPage
