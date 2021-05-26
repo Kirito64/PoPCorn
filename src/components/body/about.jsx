e71214cc87f0d87cb28e43df989566d5528a8c89
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-05-27 00:05:31 +0530

    feat: faq component

--- src/components/body/about.jsx
+++ src/components/body/about.jsx
@@ -5,7 +5,7 @@ import Logo from "../../assets/svg/logoPopcorn.svg"
 
 function About() {
 	return (
-		<div className="scontainer">
+		<div className="content">
 			<SectionHeading title= "About" />
 			<div className="about-content">
 				<div className="sidebar">
@@ -13,7 +13,7 @@ function About() {
 					<p style={{color:"white"}}>Popcorn is the very first initiative by the IIIT Nagpur with Women In Technology Community , bringing together 20+ other communities to host this hackathon, for Innovators to brainstorm and create a virtual but real prototype from June 18, 2021 - June 20, 2021. Let the Heat Pop the Code.</p>
 				</div>
 				<div className="imagee">
-					<img src ={Logo} className="abouty" />
+					<img src ={Logo} className="about" />
 				</div>
 			</div>
 		</div>
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -10,29 +10,35 @@ import About from "../components//body/about"
 import SectionHeading from "../components/body/sectionheading"
 import Faq from "../components/body/faq"
 
-const MainPage = () => (
-	<>
-		<NavBar />
-		<LandingPage />
-		<About />
-		<div className="scontainer"> 
-			<SectionHeading title= "Judges" />
-			<div className="section-wrapper team-content">
-				{judges.map((judge,idx)=>{
-					return <JudgesCard {...judge} key={idx}/>
-				})}
+const MainPage = () => {
+	
+	const question = "Test? hello"
+	const answer = "lorem ipsum dolor sit amet consectetur adipisicing 	 elit non proident d"
+	return(
+		<div className="superWrapper">
+			<NavBar />
+			<LandingPage />
+			<About />
+			<div className="content"> 
+				<SectionHeading title= "Judges" />
+				<div className="section-wrapper team-content">
+					{judges.map((judge,idx)=>{
+						return <JudgesCard {...judge} key={idx}/>
+					})}
+				</div>
+			</div>
+			<Faq question = {question} answer = {answer}/>
+			<Faq question = {question} answer = {answer}/>
+			<div className="content"> 
+				<SectionHeading title= "Team" />
+				<div className="section-wrapper team-content">
+					{teams.map((team,idx)=>{
+						return <TeamCard {...team} key={idx}/>
+					})}
+				</div>
 			</div>
 		</div>
-		<Faq />
-		<div className="scontainer"> 
-			<SectionHeading title= "Team" />
-			<div className="section-wrapper team-content">
-				{teams.map((team,idx)=>{
-					return <TeamCard {...team} key={idx}/>
-				})}
-			</div>
-		</div>
-	</>
-)
+	)
+}
 
 export default MainPage
