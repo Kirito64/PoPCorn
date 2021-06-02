66a8296cabaf59b72cb1ab16a0662f0a17251a2f
Neha Kalbande <65285162+nehakalbande@users.noreply.github.com>
2021-06-02 22:13:05 +0530

    Update teams.js

--- src/components/body/teams.js
+++ src/components/body/teams.js
@@ -1,14 +1,17 @@
 import image1 from "../../assets/team/Neha.jpg"
 import image2 from "../../assets/team/jatin.png"
 import image3 from "../../assets/team/ruchika.png"
+import image4 from "../../assets/team/parth.jpg"
+import image5 from "../../assets/team/sourav.jpg"
+import image6 from "../../assets/team/shikha.png"
 
 const teams =[
 	{
 		src:image1,
 		name:"Neha Kalbande",
 		designation:"Lead Organiser",
-		linkedin:"njb",
-		github:"njknh",
+		linkedin:"https://www.linkedin.com/in/neha-kalbande-314ab71a7/",
+		github:"https://github.com/nehakalbande",
 	},
 	{
 		src:image2,
@@ -21,29 +24,29 @@ const teams =[
 		src:image3,
 		name:"Ruchika Pandharikar",
 		designation:"Lead Organiser",
-		linkedin:"njb",
-		github:"njknh",
+		linkedin:"https://www.linkedin.com/in/ruchika-pandharikar-6033681a1/",
+		github:"https://github.com/ruchika-01p",
 	},
 	{
-		src:image2,
-		name:"Jatin Chhangani",
+		src:image4,
+		name:"Parth Jha",
 		designation:"Lead Organiser",
-		linkedin:"https://linkedin.in/jatinchhangani",
-		github:"https://github.com/Kirito64",
+		linkedin:"https://www.linkedin.com/in/parth-jha-6b501a195/",
+		github:"",
 	},
 	{
-		src:image1,
-		name:"Neha Kalbande",
+		src:image5,
+		name:"Sourav Singh",
 		designation:"Lead Organiser",
-		linkedin:"njbn",
-		github:"njknh",
+		linkedin:"https://www.linkedin.com/in/srvsingh1962/",
+		github:"https://github.com/srvsingh1962",
 	},
 	{
-		src:image2,
-		name:"Jatin Chhangani",
+		src:image6,
+		name:"Shikha Bhatt",
 		designation:"Lead Organiser",
-		linkedin:"njbn",
-		github:"njknh",
+		linkedin:"https://www.linkedin.com/in/shikha-b/",
+		github:"https://github.com/shikha-16",
 	},
 ]
 
