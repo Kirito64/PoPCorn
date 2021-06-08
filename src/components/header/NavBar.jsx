0609992c568c327b3cabf6abb5243e9576f6a4da
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-06-09 01:08:25 +0530

    working navbar fix

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import {React, useState} from "react"
 import "./NavBar.css"
 import Nav from "./nav"
 import items from "./navitems"
@@ -6,21 +6,43 @@ import {FaBars} from "react-icons/fa"
 
 
 const NavBar  = () =>{
+	const [isToggled, setToggeled] = useState(false)
 	const navItems = items.map((data, i)=>{
 		return <Nav text = {data.text} url = {data.url} key ={i}></Nav>
 	})
 
+
+	const navItems2= items.map((data, i)=>{
+		return <Nav text = {data.text} url = {data.url} key ={i} ></Nav>
+	})
+
 	const toggleNav = ()=>{
-
+		setToggeled(!isToggled)
 	}
-	return(
-		<div className="main">
-			{navItems}
+	if(!isToggled){
+		return(
+			<div className="main">
+				<div className="mobile-only" onClick = {toggleNav}>
+					<FaBars className="icon"/>
+				</div>
+				{navItems}
+			</div>	
+		)
+	}
+	else{
+		return(
+			<div className="main">
+				<div className="mobile-only" onClick = {toggleNav}>
+					<FaBars className="icon"/>
+				</div>
+				{navItems}
 
-			<div className="mobile-only nav" onClick = {toggleNav}>
-				<FaBars className="icon"/>
-			</div>
-		</div>	
-	)
+				<div className="mobile-menu">
+					{navItems2}
+				</div>
+			</div>	
+		)
+	}
+	
 }
 export default NavBar
