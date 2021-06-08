1f1cd4d46a4022b0908cce3d38d810620b1664eb
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-06-08 23:18:06 +0530

    todo: navfix

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -2,15 +2,24 @@ import React from "react"
 import "./NavBar.css"
 import Nav from "./nav"
 import items from "./navitems"
+import {FaBars} from "react-icons/fa"
+
+
 const NavBar  = () =>{
 	const navItems = items.map((data, i)=>{
 		return <Nav text = {data.text} url = {data.url} key ={i}></Nav>
 	})
+
+	const toggleNav = ()=>{
+
+	}
 	return(
 		<div className="main">
-			{/* <Nav className = "blocker"></Nav> */}
 			{navItems}
-			{/* <Nav className = "Blocker"></Nav> */}
+
+			<div className="mobile-only nav" onClick = {toggleNav}>
+				<FaBars className="icon"/>
+			</div>
 		</div>	
 	)
 }
